5a124ebbbde22675c5508d39b8eac1dcfedaf020
Limiana <5073202+Limiana@users.noreply.github.com>
2024-07-26 03:01:34 +0300

    Update

--- NightmareUI.OtterGuiWrapper/FileSystems/Configuration/ConfigFileSystemHelpers.cs
+++ NightmareUI.OtterGuiWrapper/FileSystems/Configuration/ConfigFileSystemHelpers.cs
@@ -12,7 +12,7 @@ public static class ConfigFileSystemHelpers
 		public static IEnumerable<T?> CreateInstancesOf<T>() where T:ConfigFileSystemEntry
 		{
 				var instances = typeof(T).Assembly.GetTypes().Where(x => !x.IsAbstract && typeof(T).IsAssignableFrom(x)).Select(x => (T?)Activator.CreateInstance(x, true));
-				var priorities = instances.Select(x => x.DisplayPriority);
+				var priorities = instances.Select(x => x.DisplayPriority).Distinct();
 				foreach(var x in priorities.Reverse())
 				{
 						foreach(var i in instances)
